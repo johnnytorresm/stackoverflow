5d0600de6d5bfd4a711be527c257ee60b65db938
johnnytorresm <64198505+johnnytorresm@users.noreply.github.com>
2021-09-29 18:06:53 +0200

    Create install_fasttext.py

---
+++ install_fasttext.py
@@ -0,0 +1,15 @@
+# Installing FastText - version 1.0
+
+# import os library
+import os
+
+# Cloning fastText from Facebook Research GitHub
+os.system('git clone https://github.com/facebookresearch/fastText.git')
+
+# Building the fasttext modules
+os.system('make')
+
+# Verification
+os.system('pwd')
+os.system('ls -l')
+
