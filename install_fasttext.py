3c3a08e243958a09457274f8b6fe7c0e63631af3
johnnytorresm <64198505+johnnytorresm@users.noreply.github.com>
2021-09-29 18:14:42 +0200

    Update install_fasttext.py

--- install_fasttext.py
+++ install_fasttext.py
@@ -1,7 +1,14 @@
 # Installing FastText - version 1.0
 
+# import streamlit
+import streamlit as st
+
 # import os library
 import os
+
+# Upgrade pip
+os.system('/home/appuser/venv/bin/python -m pip install --upgrade pip')
+st.write('pip has been upgraded...')
 
 # Cloning fastText from Facebook Research GitHub
 os.system('git clone https://github.com/facebookresearch/fastText.git')
@@ -13,3 +20,7 @@ os.system('make')
 os.system('pwd')
 os.system('ls -l')
 
+# scipy installation
+os.system('pip install scipy')
+
+st.write(' FastText has been installed...')
